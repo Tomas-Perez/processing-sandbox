ba73bf126ad82100df71d4a80b47da7b2e254c42
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2018-10-27 16:24:09 -0300

    Refactor ShapeUtil to use Vector2D, test

--- src/main/kotlin/com/wawey/processing/ShapeUtil.kt
+++ src/main/kotlin/com/wawey/processing/ShapeUtil.kt
@@ -1,12 +1,9 @@
 package com.wawey.processing
 
-import edu.austral.starship.base.vector.Vector2
+import com.wawey.processing.model.vector2D.Vector2Adapter
+import com.wawey.processing.model.vector2D.Vector2D
 import java.awt.Shape
-import sun.reflect.annotation.AnnotationParser.toArray
 import java.awt.geom.PathIterator
-import java.util.Arrays
-import java.util.ArrayList
-
 
 
 /**
@@ -15,28 +12,26 @@ import java.util.ArrayList
  */
 object ShapeUtil {
 
-    fun getPoints(path: Shape, flatness: Int): Array<DoubleArray> {
-        val pointList = ArrayList<DoubleArray>()
-        val coords = DoubleArray(6)
+    fun getPoints(path: Shape, flatness: Int): List<Vector2D> {
+        val pointList = mutableListOf<Vector2D>()
+        val coords = DoubleArray(2)
         var numSubPaths = 0
         val pi = path.getPathIterator(null, flatness.toDouble())
         while (!pi.isDone) {
             when (pi.currentSegment(coords)) {
                 PathIterator.SEG_MOVETO -> {
-                    pointList.add(Arrays.copyOf(coords, 2))
-                    ++numSubPaths
+                    pointList.add(vectorFromArray(coords))
+                    if (numSubPaths >= 1) throw IllegalArgumentException("Path contains multiple subpaths")
+                    else ++numSubPaths
                 }
-                PathIterator.SEG_LINETO -> pointList.add(Arrays.copyOf(coords, 2))
-                PathIterator.SEG_CLOSE -> {
-                    if (numSubPaths > 1) {
-                        throw IllegalArgumentException("Path contains multiple subpaths")
-                    }
-                    return pointList.toTypedArray()
-                }
-                else -> throw IllegalArgumentException("Path contains curves")
+                PathIterator.SEG_LINETO -> pointList.add(vectorFromArray(coords))
+                PathIterator.SEG_CLOSE -> return pointList
             }
             pi.next()
         }
         throw IllegalArgumentException("Unclosed path")
     }
+
+    private fun vectorFromArray(array: DoubleArray): Vector2D =
+            Vector2Adapter.vector(array[0].toFloat(), array[1].toFloat())
 }
--- src/test/kotlin/com/wawey/processing/ShapeUtilTest.kt
+++ src/test/kotlin/com/wawey/processing/ShapeUtilTest.kt
@@ -1,8 +1,13 @@
 package com.wawey.processing
 
+import com.wawey.processing.model.vector2D.Vector2Adapter
+import com.wawey.processing.model.vector2D.Vector2D
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import java.awt.Polygon
 import java.awt.Rectangle
+import java.awt.Shape
 
 
 /**
@@ -15,11 +20,60 @@ class ShapeUtilTest {
         @Test
         fun `gets rectangle points`() {
             val rectangle = Rectangle(0, 0, 50, 50)
-            val points = ShapeUtil.getPoints(rectangle, 0)
-            points.distinct().forEach {
-                println(it)
-            }
+            val expected = listOf(
+                    Vector2Adapter.vector(0, 0),
+                    Vector2Adapter.vector(50, 0),
+                    Vector2Adapter.vector(50, 50),
+                    Vector2Adapter.vector(0, 50),
+                    Vector2Adapter.vector(0, 0)
+            )
+
+            checkShape(rectangle, expected)
+        }
+
+        @Test
+        fun `gets rectangular polygon points`() {
+            val rectangle = Polygon(intArrayOf(0, 50, 50, 0, 0), intArrayOf(0, 0, 50, 50, 0), 5)
+            val expected = listOf(
+                    Vector2Adapter.vector(0, 0),
+                    Vector2Adapter.vector(50, 0),
+                    Vector2Adapter.vector(50, 50),
+                    Vector2Adapter.vector(0, 50),
+                    Vector2Adapter.vector(0, 0)
+            )
+
+            checkShape(rectangle, expected)
+        }
+
+        @Test
+        fun `get triangle points`() {
+            val triangle = Polygon(intArrayOf(0, 50, 0, 0), intArrayOf(0, 0, 50, 0), 4)
+            val expected = listOf(
+                    Vector2Adapter.vector(0, 0),
+                    Vector2Adapter.vector(50, 0),
+                    Vector2Adapter.vector(0, 50),
+                    Vector2Adapter.vector(0, 0)
+            )
+
+            checkShape(triangle, expected)
+        }
+
+        @Test
+        fun `get points away from origin`() {
+            val triangle = Polygon(intArrayOf(25, 50, 25, 25), intArrayOf(25, 25, 50, 25), 4)
+            val expected = listOf(
+                    Vector2Adapter.vector(25, 25),
+                    Vector2Adapter.vector(50, 25),
+                    Vector2Adapter.vector(25, 50),
+                    Vector2Adapter.vector(25, 25)
+            )
+
+            checkShape(triangle, expected)
+        }
+
+        private fun checkShape(s: Shape, l: List<Vector2D>) {
+            val points = ShapeUtil.getPoints(s, 2)
+            assertThat(points).usingElementComparator{ v1, v2 -> Vector2D.compareByXY(v1, v2) }.containsAll(l)
         }
     }
-
 }
